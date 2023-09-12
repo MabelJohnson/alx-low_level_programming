ebd03383c3391720e81020e1e5ed017b57337fe8
MabelJohnson <mabeljohnson254@gmail.com>
2023-09-12 10:40:39 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _sign - Check for lowercase character
- * @c: The character to be checked
+ * print_sign - Check for lowercase character
+ * @n: The character to be checked
  * Return: 1 for lowercase character or 0 for anything else
  */
 
