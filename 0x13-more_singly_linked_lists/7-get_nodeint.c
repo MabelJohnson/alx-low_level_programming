197c3ce0aff268ab032c8aa6947c494061880dd4
MabelJohnson <mabeljohnson254@gmail.com>
2023-10-24 14:32:58 -0700

    ebuka

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+  * get_nodeint_at_index - a function that returns the nth
+  * node of a listint_t linked list.
+  * @head: head of the list.
+  * @index: which node to stop at.
+  * Return: returns the nth
+  * node of a listint_t linked list.
+  */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+unsigned int count;
+listint_t *node;
+
+if (!head)
+return (NULL);
+
+node = head;
+for (count = 0; count < index; count++)
+{
+if (!node)
+return (NULL);
+node = node->next;
+}
+return (node);
+}
